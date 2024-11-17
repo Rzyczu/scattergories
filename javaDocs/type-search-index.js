a0bbf5dbbb42161f8da2fb4737ae6cf1d2b31020
Rzyczu <70780585+Rzyczu@users.noreply.github.com>
2024-11-17 03:24:21 +0100

    Delete javaDocs directory

--- javaDocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Client"},{"p":"org.example","l":"Game"},{"p":"org.example","l":"MenuSystem"},{"p":"org.example","l":"Player"},{"p":"org.example","l":"Server"},{"p":"org.example","l":"Game.Type"}];updateSearchResults();
